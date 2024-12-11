ce0c1dc0bfd67cc08bd1675d4ca7b9e91c99fee0
mhmd-cli <98342705+mhmd-cli@users.noreply.github.com>
2024-12-11 04:20:29 +0000

    Update Dashboard.cs

--- Dashboard.cs
+++ Dashboard.cs
@@ -22,6 +22,10 @@ namespace TC_CRM
         private Label lblWelcome;
         private Label lblUserName;
         private Label lblBenefitsStatus;
+
+        // Sample DataSet
+        public static DataSet DashboardDataset = new DataSet();
+
         public Dashboard()
         {
             InitializeComponent();
